d2ad5e9d253bf37108b25046c34841bf926b2a49
BIGduzy <nick-b@live.nl>
2018-05-08 23:58:14 +0200

    Implemented a simple interface for the motor controller

---
+++ src/MotorInterface.hpp
@@ -0,0 +1,20 @@
+#ifndef MOTOR_INTERFACE_HPP
+#define MOTOR_INTERFACE_HPP
+
+#include "wrap-hwlib.hpp"
+
+class MotorInterface {
+  public:
+    enum class INTERFACE { DC };
+
+  private:
+    const INTERFACE selectedInterface = INTERFACE::DC;
+
+  public:
+    MotorInterface(){};
+    INTERFACE getSelectedInterface() const {
+        return selectedInterface;
+    };
+};
+
+#endif
