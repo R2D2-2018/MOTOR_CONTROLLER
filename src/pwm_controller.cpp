52b06b014895e79f259493ac665eb275994377cb
StienoX <stein.bout@xs4all.nl>
2018-06-21 13:17:26 +0200

    Updated the avaible pins to the pins usable for hardware pwm. setup for PA8 is avaible. channel is also set

--- src/pwm_controller.cpp
+++ src/pwm_controller.cpp
@@ -1,37 +1,51 @@
 #include "pwm_controller.hpp"
 
 PWMcontroller::PWMcontroller(const PWMpin &pin) : pin(pin) {
+
+    uint8_t channel_id;
     switch (pin) {
-    case PWMpin::PWMpin2:
+    case PWMpin::H0_PA8:
+        PIOB->PIO_PDR = PIO_PA8;
+        PIOB->PIO_ABSR |= PIO_PA8;
+
+        // enable output on PA8
+        PIOB->PIO_OER = PIO_PA8;
+        channel_id = 0;
         break;
-    case PWMpin::PWMpin3:
+    case PWMpin::H0_PB12:
+        channel_id = 0;
         break;
-    case PWMpin::PWMpin4:
+    case PWMpin::H0_PC3:
+        channel_id = 0;
         break;
-    case PWMpin::PWMpin5:
+    case PWMpin::L0_PA21:
+        channel_id = 0;
         break;
-    case PWMpin::PWMpin6:
+    case PWMpin::L0_PB16:
+        channel_id = 0;
         break;
-    case PWMpin::PWMpin7:
-        break;
-    case PWMpin::PWMpin8:
-        break;
-    case PWMpin::PWMpin9:
-        break;
-    case PWMpin::PWMpin10:
-        break;
-    case PWMpin::PWMpin11:
-        break;
-    case PWMpin::PWMpin12:
-        break;
-    case PWMpin::PWMpin13:
+    case PWMpin::L0_PC2:
+        channel_id = 0;
         break;
     }
+    uint8_t mask = 1 << channel_id; // make mask based off channel_id, example channel_id = 2, mask is 0000 0100. enabling CHID2 in
+                                    // PWM_ENA register.
+    PWM->PWM_ENA = PWM->PWM_SR | mask; // enable channel with mask based off channel id
+    PWM->PWM_CH_NUM[channel_id].PWM_CDTY;
 };
 
-void PWMcontroller::setPwmFreq(const uint8_t &setPwmFreq) {
+void PWMcontroller::setPwmFreq(const uint8_t &setFreq) {
+    freq = setFreq;
+}
+
+void PWMcontroller::setDutyCycle(const uint8_t &setDutyCycle) {
+    dutyCycle = setDutyCycle;
 }
 
 inline uint8_t PWMcontroller::getPwmFreq() {
+    return freq;
+}
+
+inline uint8_t PWMcontroller::getDutyCycle() {
     return dutyCycle;
 }
--- src/pwm_controller.hpp
+++ src/pwm_controller.hpp
@@ -5,30 +5,68 @@
 
 // Enum class for all the avaible pwm pins on the arduino due.
 enum class PWMpin {
-    PWMpin2,
-    PWMpin3,
-    PWMpin4,
-    PWMpin5,
-    PWMpin6,
-    PWMpin7,
-    PWMpin8,
-    PWMpin9,
-    PWMpin10,
-    PWMpin11,
-    PWMpin12,
-    PWMpin13
+    // PWM_CH0
+    H0_PA8,
+    H0_PB12,
+    H0_PC3,
+    L0_PA21,
+    L0_PB16,
+    L0_PC2,
+
+    // PWM_CH1
+    H1_PA19,
+    H1_PB13,
+    H1_PC5,
+    L1_PA12,
+    L1_PB17,
+    L1_PC4,
+
+    // PWM_CH2
+    H2_PA13,
+    H2_PB14,
+    H2_PC7,
+    L2_PA20,
+    L2_PB18,
+    L2_PC6,
+
+    // PWM_CH3
+    H3_PA9,
+    H3_PB15,
+    H3_PC9,
+    L3_PA0,
+    L3_PB19,
+    L3_PC8,
+
+    // PWM_CH4
+    H4_PC20,
+    L4_PC21,
+
+    // PWM_CH5
+
+    H5_PC19,
+    L5_PC22,
+
+    // PWM_CH6
+    H6_PC18,
+    L6_PC23,
+
+    // PWM_CH7
+    L7_PC24
 };
 
 class PWMcontroller {
   private:
     uint8_t dutyCycle;
+    uint8_t freq;
     PWMpin pin;
 
   public:
     PWMcontroller(const PWMpin &pin);
 
-    void setPwmFreq(const uint8_t &setDutyCycle);
+    void setPwmFreq(const uint8_t &setFreq);
+    void setDutyCycle(const uint8_t &setDutyCycle);
     uint8_t getPwmFreq();
+    uint8_t getDutyCycle();
 };
 
 #endif // PWM_controller_HPP
