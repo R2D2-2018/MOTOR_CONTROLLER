840a05706c3fc6a6d932ce637565473710f79406
StienoX <stein.bout@xs4all.nl>
2018-06-25 00:00:57 +0200

    Moved pin adress values to switch statement

--- src/pwm_controller.cpp
+++ src/pwm_controller.cpp
@@ -4,143 +4,174 @@ PWMcontroller::PWMcontroller(const PWMpin &_pin) : pin(_pin) {
     Pio *pointerPIO = NULL;
     switch (pin) {
         // channel 1
-    case H0_RX0:
+    case H0_RX0: // <- was not declared in this scope
+        pin = PIO_PA8;
         pointerPIO = PIOA;
         channel_id = 0;
         break;
     case H0_SDA:
+        pin = PIO_PB12;
         pointerPIO = PIOB;
         channel_id = 0;
         break;
     case H0_D35:
+        pin = PIO_PC3;
         pointerPIO = PIOC;
         channel_id = 0;
         break;
     case L0_LEDTX:
+        pin = PIO_PA21;
         pointerPIO = PIOA;
         channel_id = 0;
         break;
     case L0_DAC1:
+        pin = PIO_PB16;
         pointerPIO = PIOB;
         channel_id = 0;
         break;
     case L0_D34:
+        pin = PIO_PC2;
         pointerPIO = PIOC;
         channel_id = 0;
         break;
         // channel 1
     case H1_D42:
+        pin = PIO_PA19;
         pointerPIO = PIOA;
         channel_id = 1;
         break;
     case H1_SCL:
+        pin = PIO_PB13;
         pointerPIO = PIOB;
         channel_id = 1;
         break;
     case H1_D37:
+        pin = PIO_PC5;
         pointerPIO = PIOC;
         channel_id = 1;
         break;
     case L1_RX2:
+        pin = PIO_PA12;
         pointerPIO = PIOA;
         channel_id = 1;
         break;
     case L1_A8:
+        pin = PIO_PB17;
         pointerPIO = PIOB;
         channel_id = 1;
         break;
     case L1_D36:
+        pin = PIO_PC4;
         pointerPIO = PIOC;
         channel_id = 1;
         break;
         // channel 2
     case H2_TX2:
+        pin = PIO_PA13;
         pointerPIO = PIOA;
         channel_id = 2;
         break;
     case H2_D53:
+        pin = PIO_PB14;
         pointerPIO = PIOB;
         channel_id = 2;
         break;
     case H2_D39:
+        pin = PIO_PC7;
         pointerPIO = PIOC;
         channel_id = 2;
         break;
     case L2_D43:
+        pin = PIO_PA20;
         pointerPIO = PIOA;
         channel_id = 2;
         break;
     case L2_A9:
+        pin = PIO_PB18;
         pointerPIO = PIOB;
         channel_id = 2;
         break;
     case L2_D38:
+        pin = PIO_PC6;
         pointerPIO = PIOC;
         channel_id = 2;
         break;
         // channel 3
     case H3_TX0:
+        pin = PIO_PA9;
         pointerPIO = PIOA;
         channel_id = 3;
         break;
     case H3_DAC0:
+        pin = PIO_PB15;
         pointerPIO = PIOB;
         channel_id = 3;
         break;
     case H3_D41:
+        pin = PIO_PC9;
         pointerPIO = PIOC;
         channel_id = 3;
         break;
     case L3_CANTX0:
+        pin = PIO_PA0;
         pointerPIO = PIOA;
         channel_id = 3;
         break;
     case L3_PB19:
+        pin = PIO_PB19;
         pointerPIO = PIOB;
         channel_id = 3;
         break;
     case L3_D40:
+        pin = PIO_PC8;
         pointerPIO = PIOC;
         channel_id = 3;
         break;
         // channel 4
     case H4_PC20:
+        pin = PIO_PC20;
         pointerPIO = PIOC;
         channel_id = 4;
         break;
     case L4_D9:
+        pin = PIO_PC21;
         pointerPIO = PIOC;
         channel_id = 4;
         break;
         // channel 5
     case H5_D44:
+        pin = PIO_PC19;
         pointerPIO = PIOC;
         channel_id = 5;
         break;
     case L5_D9:
+        pin = PIO_PC22;
         pointerPIO = PIOC;
         channel_id = 5;
         break;
         // channel 6
     case H6_D45:
+        pin = PIO_PC18;
         pointerPIO = PIOC;
         channel_id = 6;
         break;
     case L6_D7:
+        pin = PIO_PC23;
         pointerPIO = PIOC;
         channel_id = 6;
         break;
         // channel 7
     case L7_D6:
+        pin = PIO_PC24;
         pointerPIO = PIOC;
         channel_id = 7;
         break;
     default:
-        hwlib::cout << "Pin not implemented for pwm\n";
+        hwlib::cout << "this pwm pin is not implemented" << hwlib::endl;
         break;
     }
-    uint8_t mask = 1 << channel_id; // make mask based off channel_id, example channel_id = 2, mask is 0000 0100. enabling CHID2 in
-                                    // PWM_ENA register.
+    uint8_t mask = 1 << channel_id; // make mask based off channel_id, example channel_id = 2, mask is 0000 0100. enabling CHID2
+                                    // in PWM_ENA register.
     if (!(PWM->PWM_SR & mask)) {
         REG_PMC_PCER1 |= PMC_PCER1_PID36; // Enable PWM
 
--- src/pwm_controller.hpp
+++ src/pwm_controller.hpp
@@ -2,64 +2,55 @@
 #define PWM_controller_HPP
 
 #include "wrap-hwlib.hpp"
-
-/**
- * @file
- * @brief     This file contains the PWMpins and PWMcontroller, this code is responsable for hardware pwm.
- *
- * @author    Stein Bout & Olivier Verwoerd
- * @license   See LICENSE
- */
-
 // enum for all the avaible pwm pins on the arduino due.
-enum PWMpin : uint32_t {
+
+enum PWMpin : uint8_t {
     // PWM_CH0
-    H0_RX0 = PIO_PA8,
-    H0_SDA = PIO_PB12,
-    H0_D35 = PIO_PC3,
-    L0_LEDTX = PIO_PA21,
-    L0_DAC1 = PIO_PB16,
-    L0_D34 = PIO_PC2,
+    H0_RX0,
+    H0_SDA,
+    H0_D35,
+    L0_LEDTX,
+    L0_DAC1,
+    L0_D34,
 
     // PWM_CH1
-    H1_D42 = PIO_PA19,
-    H1_SCL = PIO_PB13,
-    H1_PC5 = PIO_PC5,
-    L1_D37 = PIO_PA12,
-    L1_A8 = PIO_PB17,
-    L1_D36 = PIO_PC4,
+    H1_D42,
+    H1_SCL,
+    H1_D37,
+    L1_RX2,
+    L1_A8,
+    L1_D36,
 
     // PWM_CH2
-    H2_TX2 = PIO_PA13,
-    H2_D53 = PIO_PB14,
-    H2_D39 = PIO_PC7,
-    L2_D43 = PIO_PA20,
-    L2_A9 = PIO_PB18,
-    L2_D38 = PIO_PC6,
+    H2_TX2,
+    H2_D53,
+    H2_D39,
+    L2_D43,
+    L2_A9,
+    L2_D38,
 
     // PWM_CH3
-    H3_TX0 = PIO_PA9,
-    H3_DAC0 = PIO_PB15,
-    H3_D41 = PIO_PC9,
-    L3_CANTX0 = PIO_PA0,
-    L3_PB19 = PIO_PB19,
-    L3_D40 = PIO_PC8,
+    H3_TX0,
+    H3_DAC0,
+    H3_D41,
+    L3_CANTX0,
+    L3_PB19,
+    L3_D40,
 
     // PWM_CH4
-    H4_PC20 = PIO_PC20, // No Physical pinout
-    L4_D9 = PIO_PC21,
+    H4_PC20, // No Physical pinout
+    L4_D9,
 
     // PWM_CH5
-
-    H5_D44 = PIO_PC19,
-    L5_D9 = PIO_PC22,
+    H5_D44,
+    L5_D9,
 
     // PWM_CH6
-    H6_D45 = PIO_PC18,
-    L6_D7 = PIO_PC23,
+    H6_D45,
+    L6_D7,
 
     // PWM_CH7
-    L7_D6 = PIO_PC24
+    L7_D6
 };
 
 class PWMcontroller {
