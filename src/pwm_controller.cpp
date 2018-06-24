9250aa047196e659a71d2d8fb52dab6a70b9b5fc
StienoX <stein.bout@xs4all.nl>
2018-06-24 11:12:15 +0200

    Added mck to channel

--- src/pwm_controller.cpp
+++ src/pwm_controller.cpp
@@ -7,17 +7,22 @@ PWMcontroller::PWMcontroller(const PWMpin &pin) : pin(pin) {
         pointerPIO = PIOB;
         channel_id = 0;
         break;
+    default:
+        hwlib::cout << "Pin not implemented for pwm\n";
+        break;
     }
-
+    hwlib::wait_ms(1);              // sets mck to 84mhz
     uint8_t mask = 1 << channel_id; // make mask based off channel_id, example channel_id = 2, mask is 0000 0100. enabling CHID2 in
                                     // PWM_ENA register.
 
-    assert(!(PWM->PWM_SR & mask)); // assert if channel is already in use.
+    // assert(!(PWM->PWM_SR & mask)); // assert if channel is already in use.
     pointerPIO->PIO_PDR = pin;
     pointerPIO->PIO_ABSR |= pin;
     pointerPIO->PIO_OER = pin;
+
+    PWM->PWM_CH_NUM[channel_id].PWM_CMR |= PWM_CMR_CPRE_MCK;
     PWM->PWM_ENA = PWM->PWM_SR | mask; // enable channel with mask based off channel id
-};
+}
 
 void PWMcontroller::setFreq(const uint8_t &setFreq) {
     freq = setFreq;
@@ -26,7 +31,12 @@ void PWMcontroller::setFreq(const uint8_t &setFreq) {
 
 void PWMcontroller::setDutyCycle(const uint16_t &setDutyCycle) {
     dutyCycle = setDutyCycle;
-    PWM->PWM_CH_NUM[channel_id].PWM_CDTY = dutyCycle;
+    // if for some reason the channel is disabled write to CDTY instead of CDTYUPD
+    if (PWM->PWM_SR & (1 << channel_id)) {
+        PWM->PWM_CH_NUM[channel_id].PWM_CDTYUPD = dutyCycle;
+    } else {
+        PWM->PWM_CH_NUM[channel_id].PWM_CDTY = dutyCycle;
+    }
 }
 
 inline uint8_t PWMcontroller::getFreq() {
