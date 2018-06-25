2dc9b0e8e1e12bd9a9dda088f375fd11ac000498
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2018-06-25 18:31:52 +0200

    update for test shells to restore files

--- test.bat
+++ test.bat
@@ -1,6 +1,16 @@
 mkdir build_test;
+cd src
+ren pwm_controller.cpp pwm_controller_back.cpp
+ren pwm_controller_mock.cpp pwm_controller.cpp
+cd ..
+timeout 1 > NUL
 cd build_test && cmake .. -Dtest_build=TRUE -G"MinGW Makefiles" && ctest -V;
 timeout 1 > NUL
+cd ..
+cd src
+ren pwm_controller.cpp pwm_controller_mock.cpp
+ren pwm_controller_back.cpp pwm_controller.cpp
+cd ..
 cd Testing/Temporary
 start LastTest.log
 pause;
--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 mkdir build_test
 cd src
+mv pwm_controller.cpp pwm_controller_backup.cpp
 mv pwm_controller_mock.cpp pwm_controller.cpp
 cd ..
 pwd
 cd build_test
 cmake .. -Dtest_build=TRUE
 ctest -V
+cd ..
+cd src
+mv pwm_controller.cpp pwm_controller_mock.cpp
+mv pwm_controller_backup.cpp pwm_controller.cpp
